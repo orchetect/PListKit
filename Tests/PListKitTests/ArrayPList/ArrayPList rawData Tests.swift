dab31d37ae742798853b2993f7dd3f06f6e2ad64
Steffan Andrews <stef@orchetect.com>
2022-08-20 22:35:48 -0700

    Added rawData tests for `ArrayPList` and `SingleValuePList`

---
+++ Tests/PListKitTests/ArrayPList/ArrayPList rawData Tests.swift
@@ -0,0 +1,59 @@
+//
+//  ArrayPList rawData Tests.swift
+//  PListKit • https://github.com/orchetect/PListKit
+//  © 2022 Steffan Andrews • Licensed under MIT License
+//
+
+#if shouldTestCurrentPlatform
+
+import XCTest
+import PListKit
+
+final class ArrayPList_rawData_Tests: XCTestCase {
+    override func setUp() { super.setUp() }
+    override func tearDown() { super.tearDown() }
+    
+    // MARK: - RawData
+    
+    func testRawData_FromXML() throws {
+        let pl = try kSamplePList.ArrayRootBasicValues.XML.plist()
+        
+        // check that rawData succeeds
+        
+        // xml
+        let rawDataXML = try pl.rawData(format: .xml)
+        let plFromXML = try ArrayPList(data: rawDataXML)
+        kSamplePList.ArrayRootBasicValues.verify(matches: plFromXML)
+        
+        // binary
+        let rawDataBinary = try pl.rawData(format: .binary)
+        let plFromBinary = try ArrayPList(data: rawDataBinary)
+        kSamplePList.ArrayRootBasicValues.verify(matches: plFromBinary)
+        
+        // openStep
+        // Apple docs:
+        // "The NSPropertyListOpenStepFormat constant is not supported for writing. It can be used only for reading old-style property lists."
+    }
+    
+    func testRawData_FromBinary() throws {
+        let pl = try kSamplePList.ArrayRootBasicValues.XML.plist()
+        
+        // check that rawData succeeds
+        
+        // xml
+        let rawDataXML = try pl.rawData(format: .xml)
+        let plFromXML = try ArrayPList(data: rawDataXML)
+        kSamplePList.ArrayRootBasicValues.verify(matches: plFromXML)
+        
+        // binary
+        let rawDataBinary = try pl.rawData(format: .binary)
+        let plFromBinary = try ArrayPList(data: rawDataBinary)
+        kSamplePList.ArrayRootBasicValues.verify(matches: plFromBinary)
+        
+        // openStep
+        // Apple docs:
+        // "The NSPropertyListOpenStepFormat constant is not supported for writing. It can be used only for reading old-style property lists."
+    }
+}
+
+#endif
---
+++ Tests/PListKitTests/SingleValuePList/SingleValuePList rawData Tests.swift
@@ -0,0 +1,59 @@
+//
+//  SingleValuePList rawData Tests.swift
+//  PListKit • https://github.com/orchetect/PListKit
+//  © 2022 Steffan Andrews • Licensed under MIT License
+//
+
+#if shouldTestCurrentPlatform
+
+import XCTest
+import PListKit
+
+final class SingleValuePList_rawData_Tests: XCTestCase {
+    override func setUp() { super.setUp() }
+    override func tearDown() { super.tearDown() }
+    
+    // MARK: - RawData
+    
+    func testRawData_FromXML() throws {
+        let pl = try kSamplePList.StringRoot.XML.plist()
+        
+        // check that rawData succeeds
+        
+        // xml
+        let rawDataXML = try pl.rawData(format: .xml)
+        let plFromXML = try SingleValuePList<String>(data: rawDataXML)
+        kSamplePList.StringRoot.verify(matches: plFromXML)
+        
+        // binary
+        let rawDataBinary = try pl.rawData(format: .binary)
+        let plFromBinary = try SingleValuePList<String>(data: rawDataBinary)
+        kSamplePList.StringRoot.verify(matches: plFromBinary)
+        
+        // openStep
+        // Apple docs:
+        // "The NSPropertyListOpenStepFormat constant is not supported for writing. It can be used only for reading old-style property lists."
+    }
+    
+    func testRawData_FromBinary() throws {
+        let pl = try kSamplePList.StringRoot.XML.plist()
+        
+        // check that rawData succeeds
+        
+        // xml
+        let rawDataXML = try pl.rawData(format: .xml)
+        let plFromXML = try SingleValuePList<String>(data: rawDataXML)
+        kSamplePList.StringRoot.verify(matches: plFromXML)
+        
+        // binary
+        let rawDataBinary = try pl.rawData(format: .binary)
+        let plFromBinary = try SingleValuePList<String>(data: rawDataBinary)
+        kSamplePList.StringRoot.verify(matches: plFromBinary)
+        
+        // openStep
+        // Apple docs:
+        // "The NSPropertyListOpenStepFormat constant is not supported for writing. It can be used only for reading old-style property lists."
+    }
+}
+
+#endif
