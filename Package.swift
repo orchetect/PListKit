52eb04092ff25a978bf21753bc5e2d19664f6e30
Steffan Andrews <stef@orchetect.com>
2022-08-22 15:59:01 -0700

    Generated docs

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     ],
     
     dependencies: [
-        // none
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0")
     ],
     
     targets: [
